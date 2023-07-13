785170bcee3d64bfea95a45020ff17da2638cc2a
Mathias Šincek <sincek.mathias@outlook.com>
2023-07-14 00:00:53 +0200

    obrisano

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main () {
+    int broj1, broj2;
+    
+    printf("Unesi brojeve:");
+    scanf("%d %d", &broj1, &broj2);
+
+    printf("Zbroj: %d", broj1+broj2);
+
+    scanf ("%d",&broj1);
+return 0;
+}
